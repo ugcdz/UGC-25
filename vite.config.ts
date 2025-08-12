8fe781b3d0b309f409d56b42c25162894459e26b
ugcdz <ugc20021@gmail.com>
2025-08-12 12:27:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+base: '/UGC-25/',
   server: {
     host: "::",
     port: 8080,
